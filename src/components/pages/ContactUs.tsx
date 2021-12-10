3b0459667b001e5da949e9982f4afe2a7b609597
Shraddha203Thorve <shraddhathorve2099@gmail.com>
2021-12-10 11:54:57 +0530

    new changes

--- src/components/pages/ContactUs.tsx
+++ src/components/pages/ContactUs.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SchoolImg from '../SchoolImg'
 import GoogleMap from './GoogleMap'
+import Card from 'react-bootstrap/Card'
 // import {
 //     Box,
 //     Container,
@@ -15,6 +16,37 @@ const ContactUs = () => {
         <div>
             <GoogleMap/>
             <p></p>
+            <div className="row">
+                    <div className="col-md-6 mt-3">
+                        <Card border="secondary" className="align shadow p-3 m-3 bg-white rounded" >
+                            {/* <Card.Header>Header</Card.Header> */}
+                            <Card.Body>
+                                <Card.Title className="brand heading">Address</Card.Title>
+                                <Card.Text>
+                                Hinjawadi Phase 1 Rd, Phase 1, Hinjewadi Rajiv Gandhi Infotech Park, Hinjawadi, Pune, Maharashtra 411057
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+
+                    {/* <hr className="clearfix w-100 d-md-none pb-0" /> */}
+
+                    <div className="col-md-6 mt-3">
+
+                    <Card border="secondary" className="align shadow p-3 m-3 bg-white rounded" >
+                            {/* <Card.Header>Header</Card.Header> */}
+                            <Card.Body>
+                                <Card.Title className="brand heading">Contact</Card.Title>
+                                <Card.Text>
+                                986004354/020-495966
+                                <br></br>
+                                icps.preprimary@learningcurve.in
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+                
             {/* <Box> */}
            
         </div>
